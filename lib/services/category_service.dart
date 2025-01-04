8e89398e38f017ba4f36206fbd04e2108aafa7f5
Baturalp Kahveci <baturalp1993@hotmail.com>
2025-01-04 12:07:58 +0300

    Some categories are filtered,added get category by id function to category service.

--- lib/services/category_service.dart
+++ lib/services/category_service.dart
@@ -5,16 +5,35 @@ import 'package:metal_gym_mobile_application/models/category.dart';
 class CategoryService {
   final String baseUrl = 'https://mgbackend-czhf.onrender.com';
 
-  // Fetch categories from backend
+  // Fetch categories from backend and filter out categories with specific slugs
   Future<List<Category>> fetchCategories() async {
     final response = await http.get(Uri.parse('$baseUrl/categories'));
 
     if (response.statusCode == 200) {
       // Parse the JSON response
       List<dynamic> data = json.decode(response.body);
-      return data.map((json) => Category.fromJson(json)).toList();
+
+      // Map the response to Category objects
+      List<Category> categories = data.map((json) => Category.fromJson(json)).toList();
+
+      // Filter out categories with the excluded slugs
+      categories.removeWhere((category) =>
+      category.slug == 'anasayfa-slider' || category.slug == 'genel' || category.slug == 'indirim');
+
+      return categories;
     } else {
       throw Exception('Failed to load categories');
+    }
+  }
+
+  // Fetch category by ID
+  Future<Category?> getCategoryById(int categoryId) async {
+    final response = await http.get(Uri.parse('$baseUrl/$categoryId'));
+
+    if (response.statusCode == 200) {
+      return Category.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load category');
     }
   }
 }
