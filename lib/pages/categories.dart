0ef23921358d3fbe8c067780f81e3113e0dac4c0
Baturalp Kahveci <baturalp1993@hotmail.com>
2025-01-04 13:59:06 +0300

    Added gradient background to category page.

--- lib/pages/categories.dart
+++ lib/pages/categories.dart
@@ -24,109 +24,128 @@ class _CategoriesPageState extends State<CategoriesPage> {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
-    final screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            // Header Section
-            Container(
-              constraints: const BoxConstraints.expand(height: 200),
-              decoration: const BoxDecoration(
-                color: Colors.black,
-                boxShadow: [
-                  BoxShadow(color: Colors.black, blurRadius: 30)
-                ],
-              ),
-              child: const Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+      body: Container(
+        // Gradient Background
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              AppColors.primary, // Start color of the gradient
+              AppColors.secondary, // End color of the gradient
+            ],
+            begin: Alignment.topCenter, // Starting point of the gradient
+            end: Alignment.bottomCenter, // Ending point of the gradient
+          ),
+        ),
+        child: Consumer<CategoryProvider>(
+          builder: (context, categoryProvider, child) {
+            return SingleChildScrollView(
+              child: Column(
                 children: [
-                  Text(
-                    "MetalGYM",
-                    style: TextStyle(
-                      color: AppColors.background,
-                      fontFamily: "Designer",
-                      fontSize: 36,
-                      shadows: [
-                        Shadow(color: Colors.white, blurRadius: 30)
+                  // Header Section
+                  Container(
+                    constraints: const BoxConstraints.expand(height: 200),
+                    decoration: const BoxDecoration(
+                      color: Colors.black,
+                      boxShadow: [
+                        BoxShadow(color: Colors.black, blurRadius: 30)
+                      ],
+                    ),
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "MetalGYM",
+                          style: TextStyle(
+                            color: AppColors.background,
+                            fontFamily: "Designer",
+                            fontSize: 36,
+                            shadows: [
+                              Shadow(color: Colors.white, blurRadius: 30)
+                            ],
+                          ),
+                        ),
+                        Text(
+                          "Fiyatlarımıza KDV dahil değildir.",
+                          style: TextStyle(
+                            color: AppColors.background,
+                            fontSize: 12,
+                          ),
+                        ),
                       ],
                     ),
                   ),
-                  Text(
-                    "Fiyatlarımıza KDV dahil değildir.",
-                    style: TextStyle(
-                      color: AppColors.background,
-                      fontSize: 12,
+                  // Sales Button Section
+                  Container(
+                    constraints: const BoxConstraints.expand(height: 50),
+                    decoration: const BoxDecoration(
+                      color: AppColors.highlighted,
+                      boxShadow: [
+                        BoxShadow(color: Colors.black, blurRadius: 15)
+                      ],
                     ),
-                  ),
-                ],
-              ),
-            ),
-            // Sales Button Section
-            Container(
-              constraints: const BoxConstraints.expand(height: 50),
-              decoration: const BoxDecoration(
-                color: AppColors.highlighted,
-                boxShadow: [
-                  BoxShadow(color: Colors.black, blurRadius: 15)
-                ],
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextButton(
-                    onPressed: () {
-                      // Implement the action for sale categories
-                    },
-                    child: const Text(
-                      "İndirimli Ürünler",
-                      style: TextStyle(
-                        color: AppColors.background,
-                        shadows: [
-                          Shadow(color: AppColors.background, blurRadius: 5),
-                          Shadow(color: AppColors.background, blurRadius: 5)
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            // Categories Grid
-            Padding(
-              padding: EdgeInsets.all(20),
-              child: Consumer<CategoryProvider>(
-                builder: (context, categoryProvider, child) {
-                  if (categoryProvider.isLoading) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (categoryProvider.categories.isEmpty) {
-                    return const Center(child: Text("No categories available"));
-                  } else {
-                    return Expanded(
-                      child: GridView.builder(
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          mainAxisSpacing: screenWidth * 0.01,
-                          crossAxisSpacing: screenWidth * 0.01,
-                          childAspectRatio: 0.65,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            // Implement the action for sale categories
+                          },
+                          child: const Text(
+                            "İndirimli Ürünler",
+                            style: TextStyle(
+                              color: AppColors.background,
+                              shadows: [
+                                Shadow(color: AppColors.background, blurRadius: 5),
+                                Shadow(color: AppColors.background, blurRadius: 5)
+                              ],
+                            ),
+                          ),
                         ),
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(), // Prevent scrolling for parent widget
-                        itemCount: categoryProvider.categories.length,
-                        itemBuilder: (context, index) {
-                          final category = categoryProvider.categories[index];
-                          return CategoryBox(
-                            category: category,
-                          );
-                        },
+                      ],
+                    ),
+                  ),
+                  // Main Content Section
+                  categoryProvider.isLoading
+                      ? SizedBox(
+                    height: MediaQuery.of(context).size.height - 250,
+                    child: const Center(child: CircularProgressIndicator()),
+                  )
+                      : categoryProvider.categories.isEmpty
+                      ? SizedBox(
+                    height: MediaQuery.of(context).size.height - 250,
+                    child: const Center(
+                      child: Text(
+                        "No categories available",
+                        style: TextStyle(color: Colors.white),
                       ),
-                    );
-                  }
-                },
+                    ),
+                  )
+                      : Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        mainAxisSpacing: screenWidth * 0.01,
+                        crossAxisSpacing: screenWidth * 0.01,
+                        childAspectRatio: 0.65,
+                      ),
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(), // Prevent scrolling for parent widget
+                      itemCount: categoryProvider.categories.length,
+                      itemBuilder: (context, index) {
+                        final category = categoryProvider.categories[index];
+                        return CategoryBox(
+                          category: category,
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
